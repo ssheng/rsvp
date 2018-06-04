2741ed17694de7c974b3517c2b75f10999ff1eae
Sean Sheng <ssheng@linkedin.com>
2018-06-04 02:03:31 -0700

    Font awesome CDN

--- index.php
+++ index.php
@@ -7,7 +7,7 @@ use Rsvp\DataModel\Datastore;
 
 //$id = ltrim($_SERVER['REQUEST_URI'], '/');
 $datastore = new Datastore('wedding-rsvp-201609');
-$rsvp = $datastore->read('83qnAk4WjEuf2xOCyFap1w==');
+$rsvp = $datastore->read('wyJ5U22I1EGSTT89XvYZ9w==');
 
 $partySelect = array('', '', '', '');
 $sailSelect = array('', '');
@@ -44,8 +44,7 @@ if ($rsvp) {
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
   
   <!-- Custom fonts for this template -->
-  <link href="vendor/font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
-  <!-- <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.0.13/css/all.css" integrity="sha384-DNOHZ68U8hZfKXOrtjWvjxusGo9WQnrNx2sqG0tfsghAvtVlRW3tvkXWZh58N9jp" crossorigin="anonymous"> -->
+  <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.0.13/css/all.css" integrity="sha384-DNOHZ68U8hZfKXOrtjWvjxusGo9WQnrNx2sqG0tfsghAvtVlRW3tvkXWZh58N9jp" crossorigin="anonymous">
   <link href='https://fonts.googleapis.com/css?family=Droid+Serif:400,700,400italic,700italic' rel='stylesheet' type='text/css'>
   <link href="https://fonts.googleapis.com/css?family=Lato|Roboto" rel="stylesheet" type='text/css'>
   <link href="https://fonts.googleapis.com/css?family=Pinyon+Script" rel="stylesheet" type='text/css'>
@@ -359,10 +358,10 @@ if ($rsvp) {
     </div>
   </footer>
   <!-- Bootstrap core JavaScript -->
-  <script src="vendor/jquery/jquery.min.js"></script>
-  <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+  <script src="https://storage.googleapis.com/rsvp-resources/jquery.min.js"></script>
+  <script src="https://storage.googleapis.com/rsvp-resources/bootstrap.bundle.min.js"></script>
   <!-- Plugin JavaScript -->
-  <script src="vendor/jquery-easing/jquery.easing.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-easing/1.4.1/jquery.easing.min.js"></script>
   <!-- Contact form JavaScript -->
   <script src="js/jqBootstrapValidation.js"></script>
   <script src="js/contact_me.js"></script>
